47d580bc88fdf6bfcefa0c2831eaa9f6bd9768ac
ChooiLi <yeechooili1120@gmail.com>
2021-12-14 06:24:42 +0800

    add createEST

--- app/src/main/java/com/example/share4care/chooili/AdminHome.kt
+++ app/src/main/java/com/example/share4care/chooili/AdminHome.kt
@@ -22,90 +22,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_admin_home)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.nav_drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            R.string.nav_drawer_open,
-            R.string.nav_drawer_close
-        )
-        toggle.isDrawerIndicatorEnabled=true
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-        navView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-
-                R.id.admin_home -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Home",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                R.id.admin_verifyUser-> Toast.makeText(
-                    applicationContext,
-                    "Clicked Verify User",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-
-                R.id.admin_verifyEST
-                -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Verify EST",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                R.id.admin_delAcc
-                -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Delete Account",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                R.id.admin_viewReport
-                -> Toast.makeText(
-                    applicationContext,
-                    "Clicked View Report",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                R.id.admin_createEST
-                -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Create EST",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-                R.id.admin_ESTdetails
-                -> Toast.makeText(
-                    applicationContext,
-                    "Clicked EST details",
-                    Toast.LENGTH_SHORT
-                ).show()
-
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            //After type true, all error gone
-            true
+        findViewById<CardView>(R.id.cvCreateEST).setOnClickListener(){
+           val intent = Intent(this@MainActivity, CreateESTActivity::class.java)
+            startActivity(intent)
         }
+
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        if (toggle.onOptionsItemSelected(item)) {
-            return true
-
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
